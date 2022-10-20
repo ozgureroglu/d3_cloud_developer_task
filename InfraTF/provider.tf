bec749751d0c5fe3cf3cc77f57e91bb88344d076
Özgür Eroğlu <qubitera@gmail.com>
2022-10-20 18:09:47 +0300

    folder structure for the projects created

---
+++ InfraTF/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+
+  backend "s3" {
+    bucket = "tfbackend-oe"
+    key    = "state/example2.tfstate"
+    region = "eu-central-1"
+  }
+}
+
+# Configure AWS provider:
+provider "aws" {
+  region  = "us-east-1"
+}
